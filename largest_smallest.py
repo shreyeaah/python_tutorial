3bbe7a0b93f1bcdc1d92368faf73fbb5735c8e67
shreyeaah <121953123+shreyeaah@users.noreply.github.com>
2025-02-04 21:59:56 +0530

    Delete largest_smallest.py

--- largest_smallest.py
+++
@@ -1,9 +0,0 @@
-lst = []  
-n = int(input("Enter number of elements: "))  
-
-for i in range(n):  
-    num = int(input(f"Enter element {i + 1}: "))  
-    lst.append(num)  
-
-lst.sort()  
-print(f"Largest element is : {lst[-1]}, Smallest  element is : {lst[0]}")
